8baddeb7e774bca601b21011af674df5e1f96a28
dashrath077 <kumardashrath199@gmail.com>
2020-07-09 11:29:55 +0530

    first commit

---
+++ my-app/src/homepage.component.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+
+const Homepage = () => (
+    <div class="homepage">
+        <div class="directory-menu">
+            <div class="menu-item">
+                <div class="content">
+                   <h1 class="title">Hats</h1>
+                   <span class="subtittle">Shop Now</span>
+                </div>
+            </div>
+            <div class="menu-item"> 
+                <div class="content">
+                   <h1 class="title">Sneakers</h1>
+                   <span class="subtittle">Shop Now</span>
+                </div>
+            </div>
+            <div class="menu-item">
+                <div class="content">
+                   <h1 class="title">Womens</h1>
+                   <span class="subtittle">Shop Now</span>
+                </div>
+            </div>
+            <div class="menu-item">
+                <div class="content">
+                   <h1 class="title">Mens</h1>
+                   <span class="subtittle">Shop Now</span>
+                </div>
+            </div>
+        </div>
+    </div>
+);
+
+export default Homepage;
